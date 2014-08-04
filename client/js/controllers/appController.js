fa9c699daf4cfe4d0450be5da4c1a86aa9bfc6b0
Arhan <arhanshahid@gmail.com>
2014-08-04 04:08:40 -0700

    go Home Function implemented

--- client/js/controllers/appController.js
+++ client/js/controllers/appController.js
@@ -1,26 +1,42 @@
 contactApp.controller('appController',function($rootScope,$location,$localStorage){
+
     $rootScope.user = {
         isLogin : false,
         name:null,
         password:null
     };
+
     $rootScope.logOut = function(){
         $rootScope.user = {
             isLogin : false,
             name:null,
             password:null
         };
+        delete $localStorage.user;
         $location.path('/home/login');
+    };
+
+    $rootScope.goToHome = function(){
+        var user = $localStorage.user;
+       if(user != undefined) {
+           if (user.name != null && user.password != null) {
+               $location.path('contact');
+           }
+       }else{
+           $location.path('/home/login');
+       }
     };
 
     function refresh(){
         var user = $localStorage.user;
-        if(user.name !=null && user.password!=null){
-            $rootScope.user = {
-                isLogin : user.isLogin,
-                name:user.name,
-                password:user.password
-            };
+        if(user != undefined){
+            if(user.name !=null && user.password!=null){
+                $rootScope.user = {
+                    isLogin : user.isLogin,
+                    name:user.name,
+                    password:user.password
+                };
+            }
         }
     }
     refresh();
