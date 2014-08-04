08f0e13dc9276807a313f8c3dd62ecb58f040015
Arhan <arhanshahid@gmail.com>
2014-08-04 05:42:02 -0700

    Add Contact UI Set

--- client/js/controllers/addController.js
+++ client/js/controllers/addController.js
@@ -1,3 +1,17 @@
-contactApp.controller('addController',function($rootScope,$scope){
+contactApp.controller('addController',function($rootScope,$scope,$location,alertService){
     $scope.addContact = "Add Contact";
+    $scope.saveContact = function(contact){
+
+        var contactValid = contact != undefined && contact.name && contact.phone;
+        if(contactValid){
+            console.log('contact');
+            console.log(contact);
+            alertService.show("Contact Saved", "success");
+        }else{
+            alertService.show("Empty Field", "danger");
+        }
+    };
+    $scope.goToContact =function(){
+        $location.path('contact');
+    }
 });
