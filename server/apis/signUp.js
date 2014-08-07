c24c4986f8f8017af176f9a7abf013dd2eaaa4b9
Arhan <arhanshahid@gmail.com>
2014-08-07 01:11:34 -0700

    UserId Validation Set

--- server/apis/signUp.js
+++ server/apis/signUp.js
@@ -5,37 +5,35 @@ exports = module.exports = function(app, mongoose) {
         app.api.callback.res = res;
         var validationNotEmpty = req.body.name && req.body.email && req.body.dob && req.body.password;
         var validationNotUndefined = req.body.name != undefined && req.body.email != undefined && req.body.dob != undefined && req.body.password != undefined;
-
-//        if(app.api.regexStringValidator(req.body.name.toLocaleString())){
-//
-//
-//
-//        }
-
-
+        var userName = req.body.name.toLowerCase();
+        var validationUserName = app.api.regexStringValidator(userName);
 
         if(validationNotEmpty && validationNotUndefined){
-            console.log('SignUp From Server Work - Log');
 
-            var user = new app.db.models.User({
-                name:req.body.name,
-                email:req.body.email,
-                dob:req.body.dob,
-                password:req.body.password
-            });
+            if (validationUserName) {
+                console.log('SignUp From Server Work - Log');
+                var user = new app.db.models.User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    dob: req.body.dob,
+                    password: req.body.password
+                });
 
-            user.save(function(err,responce){
-                if (err) {
-                    res.send(err);
-                }
-                app.api.callback(err,responce);
-            });
-        }
-        else{
-            console.log('Empty or Invalid Data Field - Log');
+                user.save(function (err, responce) {
+                    if (err) {
+                        res.send(err);
+                    }
+                    app.api.callback(err, responce);
+                });
+            }
+            else {
+                console.log("Invalid ID format, Please follow the instruction to create ID - Log");
+                app.api.callback("Invalid ID format, Please follow the instruction to create ID");
+            }
+        }else {
+            console.log("Empty or Invalid Data Field - Log");
             app.api.callback("Empty or Invalid Data Field");
         }
-
     };
 
 };
