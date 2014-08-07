5f5f5dddeac1f6dd35ca3ef5b57fb13f675827cf
Arhan <arhanshahid@gmail.com>
2014-08-07 05:19:39 -0700

    Get And Add Contact Api Works

--- server/apis/getContact.js
+++ server/apis/getContact.js
@@ -1,7 +1,37 @@
 exports = module.exports = function(app, mongoose) {
 
     app.api.getContact = function(req, res) {
-        console.log("Get Contact");
+
+        app.api.callback.res = res;
+
+        var validationNotEmpty = req.params.userId;
+        var validationNotUndefined = req.params.userId != undefined;
+
+        if(validationNotEmpty && validationNotUndefined){
+
+            app.db.models.User.findOne({_id:req.params.userId},function(err,userObject){
+                if(err){
+                    app.api.callback(err);
+                }
+                else if(!(userObject && userObject._id)){
+                    console.log('Invalid User or Password - Log');
+                    app.api.callback("Invalid User or Password");
+                }else{
+
+                    app.db.models.Contact.find({owner:userObject._id},function(err,contacts) {
+                        if (err) {
+                            app.api.callback(err);
+                        }
+                        app.api.callback(err, contacts);
+                    });
+                }
+            });
+        }
+        else{
+            console.log('Empty or Invalid Data Field - Log');
+            app.api.callback("Empty or Invalid Data Field");
+        }
+
     };
 
 };
